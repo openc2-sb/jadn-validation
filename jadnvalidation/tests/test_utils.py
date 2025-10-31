9ca01703cb67eab6597915dc8a632af187cad85c
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-10-31 15:33:13 -0400

    added external util call test

--- jadnvalidation/tests/test_utils.py
+++ jadnvalidation/tests/test_utils.py
@@ -1,5 +1,6 @@
 from jadnvalidation.data_validation.data_validation import DataValidation
 from jadnvalidation.utils.consts import JSON
+from jadnutils.utils.jadn_utils import get_inherited_fields
 
 
 def validate_valid_data(j_schema: dict, root: str, data_list: list, data_format: str = JSON) -> int:
@@ -52,3 +53,36 @@ class Utils:
                 err_count = err_count + 1
                 print(err)
         return err_count
+    
+ # tests hitting jadnutils function    
+def test_get_inherited_fields():
+    """Test get_inherited_fields function."""
+    
+    sample_schema = {
+        "meta": {
+            "title": "Test Schema",
+            "version": "1.0"
+        },
+        "types": [
+            ["Person", "Record", [], "A person", [
+                [1, "name", "String", [], "Person's name"],
+                [2, "age", "Integer", [], "Person's age"],
+                [3, "email", "String", [], "Optional email"]
+            ]],
+            ["Company", "Record", [], "A company", [
+                [1, "name", "String", [], "Company name"],
+                [2, "employees", "Person-Array", [], "List of employees"]
+            ]],
+            ["Person-Array", "ArrayOf", ["{*Person"], "Array of persons"],
+            ["Status", "Enumerated", [], "Status values", [
+                [1, "active", "Active status"],
+                [2, "inactive", "Inactive status"]
+            ]]
+        ]
+    }
+    
+    types = sample_schema["types"]
+    field = ["Person", "Record", [], "A person"]
+    result = get_inherited_fields(types, field)
+    # This function returns empty list when no inheritance options are found
+    assert isinstance(result, list)    
