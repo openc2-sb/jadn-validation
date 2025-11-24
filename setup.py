0a7cd3b4cc0dafaf4a4827069d12a5b97cfbc801
Matt Roberts <matthew.roberts@bestgateeng.com>
2025-11-24 12:27:43 -0500

    bumping back down to 2.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
  
 setup(
     name="jadnvalidation",
-    version="2.6.0", 
+    version="2.5.1", 
     packages=["jadnvalidation", "jadnvalidation.data_validation", "jadnvalidation.data_validation.formats", "jadnvalidation.data_validation.schemas", "jadnvalidation.models", "jadnvalidation.models.jadn", "jadnvalidation.utils", "jadnvalidation.models.jadn"],
     install_requires=[
         "jsonpointer",
